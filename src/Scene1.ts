5ae255073e383a2adf5bc98f5c130624255275e4
Lior Arbel <larbel@netflix.com>
2025-10-04 02:45:05 +0300

    Add Runge Kutta

--- src/Scene1.ts
+++ src/Scene1.ts
@@ -54,10 +54,44 @@ export class Scene1 extends PhlysicsScene<Scene1Props, Scene1Variables> {
         const c = this.useStore.getState() as typeof this.constants;
         this.constants = c;
 
-        const displacement = this.cube.position.x - c.cubeRest;
-        this.variables.cubeVelocity += d * (-c.springConstant * displacement / c.cubeMass);
-        this.variables.cubeVelocity += -this.variables.cubeVelocity * c.damping;
-        this.cube.position.x += d * this.variables.cubeVelocity;
+        // f = ma f=-kx => x = -f/k = -ma/k
+        function derivative(x: number, v: number) {
+            const displacement = x - c.cubeRest;
+            const acceleration = -c.springConstant * displacement / c.cubeMass - c.damping * v;
+            return [v, acceleration];
+        }
+
+        const x = this.cube.position.x, v = this.variables.cubeVelocity;
+
+        const method: string = "RK4";
+
+        if (method === 'RK1') {
+            const [dx1, dv1] = derivative(x, v);
+
+            this.cube.position.x += dx1 * d;
+            this.variables.cubeVelocity += dv1 * d;
+        }
+        if (method === 'RK2') {
+            const [dx1, dv1] = derivative(x, v);
+            const [dx2, dv2] = derivative(x + d * dx1, v + d * dv1);
+
+            this.cube.position.x += (dx1 + dx2) * d / 2;
+            this.variables.cubeVelocity += (dv1 + dv2) * d / 2;
+        }
+        if (method === 'RK4') {
+            const [dx1, dv1] = derivative(x, v);
+            const [dx2, dv2] = derivative(x + 0.5 * d * dx1, v + 0.5 * d * dv1);
+            const [dx3, dv3] = derivative(x + 0.5 * d * dx2, v + 0.5 * d * dv2);
+            const [dx4, dv4] = derivative(x + d * dx3, v + d * dv3);
+
+            this.cube.position.x += (d / 6) * (dx1 + 2 * dx2 + 2 * dx3 + dx4);
+            this.variables.cubeVelocity += (d / 6) * (dv1 + 2 * dv2 + 2 * dv3 + dv4);
+        }
+
         this.spring.scale.z = -this.cube.position.x + this.constants.cubeStart - 0.5;
     }
+}
+
+function eulerSolver(initial0: number, initial1: number, update: (d: number, f: number, ft: number) => void) {
+
 }
